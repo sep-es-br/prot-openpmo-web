7987a83f276932f444c898ad953a67e4ae0d6303
Vagner Cordeiro <vagner@MacBook-Air-de-Vagner.local>
2018-08-04 18:23:40 -0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
