7b4046c08051497e8bb8088179288b346ef7dab6
Vagner Dargan Cordeiro <vagner.cordeiro@pepgg12z.planejamento.es.gov.br>
2018-10-19 16:22:55 -0300

    Adding properties to Workpack Template

--- src/app/model/property.ts
+++ src/app/model/property.ts
@@ -1,8 +1,8 @@
 export class Property {
-    id: String;
-    typeName: String;
-    name: String;
-    max: Number;
-    min: Number;
+    id: String = '';
+    typeName: String = '';
+    name: String = '';
+    max: Number = -1;
+    min: Number = -1;
     value: any;
 }
