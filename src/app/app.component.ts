b600eedb1b50deb0c11cad214baf2e40e8dbcf9d
Vagner Dargan Cordeiro <vagner.cordeiro@pepgg12z.planejamento.es.gov.br>
2018-11-28 16:07:29 -0200

    Adjustments to deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { TranslateConstants } from '../app/model/translate';
 
 @Component({
   selector: 'app-root',
